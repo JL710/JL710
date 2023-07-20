70e9fe572b2cdc485b5b9c3746557dcebae63d11
JL710 <76447362+JL710@users.noreply.github.com>
2023-07-20 22:37:42 +0200

    cleanup

--- Documentation/documentation-home.js
+++
@@ -1,5 +0,0 @@
-function ScrollUp() {
-    console.log("scrollup")
-    document.body.scrollTop = 0; // for safari
-    document.documentElement.scrollTop = 0; //for For Chrome, Firefox, IE and Opera
-}
